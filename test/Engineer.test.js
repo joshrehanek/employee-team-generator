535aaae2addf74041773131951cb315f63b5d64c
Josh Rehanek <joshrehanek@gmail.com>
2021-01-27 01:46:16 -0800

    adds engineer new github test

--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -1,19 +1,29 @@
 const Engineer = require("../lib/Engineer");
 
-test("Can set GitHUb account via constructor", () => {
-  const testValue = "GitHubUser";
-  const e = new Engineer("Foo", 1, "test@test.com", testValue);
-  expect(e.github).toBe(testValue);
+describe("Engineer", () => {
+  describe("github", () => {
+    it("should recognize a new engineer github username has been generated", () => {
+      const github = "joshrehanek";
+      const engineerGithub = new Engineer ("Josh", 123, "josh@gmail.com", github)
+      expect(engineerGithub.github).toEqual(github);
+    });
+  });
+  
 });
+// test("Can set GitHUb account via constructor", () => {
+//   const testValue = "GitHubUser";
+//   const e = new Engineer("Foo", 1, "test@test.com", testValue);
+//   expect(e.github).toBe(testValue);
+// });
 
-test("getRole() should return \"Engineer\"", () => {
-  const testValue = "Engineer";
-  const e = new Engineer("Foo", 1, "test@test.com", "GitHubUser");
-  expect(e.getRole()).toBe(testValue);
-});
+// test("getRole() should return \"Engineer\"", () => {
+//   const testValue = "Engineer";
+//   const e = new Engineer("Foo", 1, "test@test.com", "GitHubUser");
+//   expect(e.getRole()).toBe(testValue);
+// });
 
-test("Can get GitHub username via getGithub()", () => {
-  const testValue = "GitHubUser";
-  const e = new Engineer("Foo", 1, "test@test.com", testValue);
-  expect(e.getGithub()).toBe(testValue);
-});
+// test("Can get GitHub username via getGithub()", () => {
+//   const testValue = "GitHubUser";
+//   const e = new Engineer("Foo", 1, "test@test.com", testValue);
+//   expect(e.getGithub()).toBe(testValue);
+// });
